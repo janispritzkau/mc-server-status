43f13359eeb1241d7c72794a2a057474896ac3d4
Janis Pritzkau <janispritzkau@gmail.com>
2020-09-16 19:33:24 +0200

    Add timeout option and update version to 2.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, PacketWriter } from "mcproto"
+import { Client, PacketWriter, State } from "mcproto"
 
 export interface Status {
     version: {
@@ -14,17 +14,35 @@ export interface Status {
     ping?: number
 }
 
-export const getStatus = async (host: string, port?: number, checkPing = true): Promise<Status> => {
-    const client = await Client.connect(host, port)
+export interface StatusOptions {
+    /** @default true */
+    checkPing?: boolean
+    /** @default 10000 // ms */
+    timeout?: number
+}
 
-    client.send(new PacketWriter(0x0).writeVarInt(404)
-        .writeString(host).writeUInt16(client.socket.remotePort!).writeVarInt(1))
+const defaultOptions: Partial<StatusOptions> = {
+    checkPing: true,
+    timeout: 5000
+}
+
+export async function getStatus(host: string, port?: number | null, options?: StatusOptions): Promise<Status> {
+    options = { ...defaultOptions, ...options }
+
+    const client = await Client.connect(host, port, {
+        connectTimeout: options.timeout,
+        timeout: options.timeout
+    })
+
+    client.send(new PacketWriter(0x0).writeVarInt(498)
+        .writeString(host).writeUInt16(client.socket.remotePort!)
+        .writeVarInt(State.Status))
 
     client.send(new PacketWriter(0x0))
 
     const status: Status = (await client.nextPacket()).readJSON()
 
-    if (checkPing) {
+    if (options.checkPing) {
         client.send(new PacketWriter(0x1).write(Buffer.alloc(8)))
         const start = Date.now()
 
